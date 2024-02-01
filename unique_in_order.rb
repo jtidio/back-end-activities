509af7ee93f642052079dd1193acaaf55ee34fd0
jtidio <145768126+jtidio@users.noreply.github.com>
2024-02-01 19:46:59 +0800

    unique in order

---
+++ unique_in_order.rb
@@ -0,0 +1,30 @@
+def unique_in_order (input)
+    if input.is_a?(Array)
+    order = []
+    input.each do |item|
+        if order[order.length-1] == item
+            next
+        else 
+            order.push(item)
+        end
+    end
+    puts "Unique in Order: #{order}"
+
+    elsif input.is_a?(String)
+    letters_array = input.each_char.to_a
+    order = []
+
+    letters_array.each do |item|
+        if order[order.length-1] == item
+            next
+        else 
+            order.push(item)
+        end
+    end
+    puts "Unique in Order: #{order}"
+    end
+end
+
+unique_in_order("AAAABBBCCDAABBB")
+unique_in_order("ABBCcAD")
+unique_in_order([1,2,2,3])
